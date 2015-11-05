e46f6446b496b592acd17bc69bfb4cfa3926ab42
TobbetheStalker <tobbe.kling@hotmail.com>
2015-11-05 14:02:56 +0100

    Some fixes
    fixes

--- VSProject/VSProject/main.cpp
+++ VSProject/VSProject/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+using namespace std;
 int main (){
-	std::cout << "Hello" << std::endl;
+	cout << "Hello" << endl;
+	bool i = false;
+	if (i == true){
+		cout << "i = true" << endl;
+	}
+	else{
+		cout << "i = false" << endl;
+	}
+
+	cout << "End of this file" << endl;
 }
